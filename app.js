633e16325368c4cf4546a496a5b54e0a6a7d4c70
Joshua Heideman <jcheideman@ucdavis.edu>
2019-02-19 19:01:59 -0800

    Have player do regular and special attacks against enemy.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ new Vue({
 		GameStart:false,
 		PlayerHealth:100,
 		EnemyHealth:100,
+		EnemyBar: '200px'
 	},
 	methods:{
 		StartGame: function(){
@@ -14,6 +15,17 @@ new Vue({
 			this.GameStart = false;
 			this.PlayerHealth = 100;
 			this.EnemyHealth = 100;
+			this.EnemyBar = "200px";
+		},
+		Attack: function(){
+			const attack_val = Math.floor(Math.random()*10)+1;
+			this.EnemyHealth = this.EnemyHealth - attack_val;
+			this.EnemyBar = this.EnemyHealth*2 + "px";
+		},
+		SpecialAttack: function(){
+			const attack_val = Math.floor(Math.random()*10)+11;
+			this.EnemyHealth = this.EnemyHealth - attack_val;
+			this.EnemyBar = this.EnemyHealth*2 + "px";
 		}
 	}
 })
