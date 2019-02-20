1948738717013e0567db6375167462b778bbffac
Joshua Heideman <jcheideman@ucdavis.edu>
2019-02-19 19:10:57 -0800

    Make enemy attack player

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
 	{
 		GameStart:false,
 		PlayerHealth:100,
+		PlayerBar:'200px',
 		EnemyHealth:100,
 		EnemyBar: '200px'
 	},
@@ -14,6 +15,7 @@ new Vue({
 		Reset: function(){
 			this.GameStart = false;
 			this.PlayerHealth = 100;
+			this.PlayerBar = "200px"
 			this.EnemyHealth = 100;
 			this.EnemyBar = "200px";
 		},
@@ -21,11 +23,18 @@ new Vue({
 			const attack_val = Math.floor(Math.random()*10)+1;
 			this.EnemyHealth = this.EnemyHealth - attack_val;
 			this.EnemyBar = this.EnemyHealth*2 + "px";
+			this.EnemyAttack();
 		},
 		SpecialAttack: function(){
 			const attack_val = Math.floor(Math.random()*10)+11;
 			this.EnemyHealth = this.EnemyHealth - attack_val;
 			this.EnemyBar = this.EnemyHealth*2 + "px";
+			this.EnemyAttack();
+		},
+		EnemyAttack: function(){
+			const attack_val = Math.floor(Math.random()*20)+1;
+			this.PlayerHealth = this.PlayerHealth - attack_val;
+			this.PlayerBar = this.PlayerHealth*2 + "px";
 		}
 	}
 })
